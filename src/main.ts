fad662baed0bf562d70abf0fa4ceafc57e5c204f
Jaseel <jaseel@MAGs-Mac-mini.local>
2026-03-18 10:47:45 +0400

    Allow cors for all

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: '*', // allow all (for testing)
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
